516383d49c8ef0443ea0df2eefbd6870e0cc272c
Faith Magras <bdanielle092@gmail.com>
2020-09-17 11:49:53 -0500

    added profile for owner

--- DogGo/Repositories/IDogRepository.cs
+++ DogGo/Repositories/IDogRepository.cs
@@ -9,10 +9,10 @@ namespace DogGo.Repositories
     public interface IDogRepository
     {
         List<Dog> GetAllDogs();
+        List<Dog> GetDogsByOwnerId(int ownerId);
 
         Dog GetDogById(int id);
-        Dog GetDogsByOwnerId(int ownerId);
-
+       
         void AddDog(Dog dog);
         void UpdateDog(Dog dog);
         void DeleteDog(int dogId);
